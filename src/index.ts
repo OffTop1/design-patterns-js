fe193eab90b44c282278f02534f3fcaa676086c2
Kuyoshbek Mekhmonov <Kuyoshbek_Mekhmonov@epam.com>
2021-07-02 14:43:00 +0500

    feat: add the implementation of the Observer pattern

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,11 @@ console.log(pilot.__proto__, 'pilot');
 /* Constructor Pattern -- END */
 
 
+
+
+
+
+
 /* Module Pattern -- START */
 
 import { testModule, basketModule } from './patterns/_module';
@@ -41,6 +46,10 @@ scope of the basketModule console.log(basket); */
 /* Module Pattern -- END */
 
 
+
+
+
+
 /* The Singleton Pattern -- START */
 
 import { SingletonTester } from './patterns/singleton';
@@ -57,3 +66,61 @@ the difficulty in creating multiple instances, difficul-
 ty in stubbing dependencies and so on. */
 
 /* The Singleton Pattern -- END */
+
+
+
+
+
+/* The Observer Pattern -- START */
+import { Subject, Observer, IObserver, ISubject } from './patterns/observer';
+
+interface IInputObserver extends HTMLInputElement, Partial<ISubject> {}
+
+// Extend object with extension
+function extend(obj: any, extension: any) {
+    for(let key in extension) {
+        obj[key] = extension[key];
+    }
+}
+
+// Reference to our DOM elements
+let controlCheckbox: IInputObserver = document.querySelector('.main-checkbox');
+let addBtn: HTMLButtonElement = document.querySelector('.add-new-observer');
+let container: HTMLDivElement = document.querySelector('.observers-container');
+
+// Concrete Subject
+// Extend the controlling checkbox with the Subject class
+extend(controlCheckbox, new Subject());
+
+// Clicking the checkbox will trigger notifications to its
+// observers
+controlCheckbox.onclick = function() {
+    controlCheckbox.notify(controlCheckbox.checked);
+}
+
+addBtn.onclick = addNewObserver;
+
+// Concrete Observer
+
+function addNewObserver() {
+    // Create a new checkbox to be added
+    let check: HTMLInputElement & Partial<IObserver> = document.createElement('input');
+    check.type = 'checkbox';
+
+    // Extend the checkbox with the Observer class
+    extend(check, new Observer());
+
+    // Override with custom behaviour
+    check.update = function(value: boolean) {
+        this.checked = value;
+        console.log('checked?: ', value);
+    }
+
+    // Add the new Observer to our list of observers
+    // for our main subject
+    controlCheckbox.addObserver(check as IObserver);
+
+    // Append the item to the container
+    container.appendChild(check);
+}
+/* The Observer Pattern -- END */
